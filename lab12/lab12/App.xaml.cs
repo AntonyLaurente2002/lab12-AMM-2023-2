cdcd895f78ea06205c846526dd6e54817ac10c9e
AntonyLaurente2002 <antony.laurente@tecsup.edu.pe>
2023-12-02 01:36:58 -0500

    subiendo cambios

--- lab12/lab12/App.xaml.cs
+++ lab12/lab12/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using lab12.View;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace lab12
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new TaskView();
         }
 
         protected override void OnStart()
